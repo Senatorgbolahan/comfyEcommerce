dbc8275b17e545f7bb0903791ae407eebfe6ce11
Senatorgbolahan <efunkoyaomogbolahan@gmail.com>
2022-10-12 00:43:06 +0100

    comfy is an e-commerce application developed with html,css, and javascript

---
+++ src/fetchProducts.js
@@ -0,0 +1,12 @@
+import { allProductsUrl } from './utils.js';
+
+const fetchProducts = async() => {
+    const response = await fetch(allProductsUrl)
+        .catch((err) => {
+            console.log(err);
+        })
+    console.log(response);
+    return response;
+};
+
+export default fetchProducts;
